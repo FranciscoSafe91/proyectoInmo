17bc17e51fb33fb1830a29b63d273538ed046aa5
FranciscoSafe91 <francisco.javier.safe@gmail.com>
2026-09-10 01:45:18 -0300

    esta si es la buena buena

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../backend/public/app',
+    outDir: 'dist',
     emptyOutDir: true,
   },
   server: {
